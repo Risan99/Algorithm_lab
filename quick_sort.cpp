0435c39719d0ba9203b911a79260c857790fc0a9
Risan Mahfuz <102344404+Risan99@users.noreply.github.com>
2023-07-08 07:33:41 +0600

    Update quick_sort.cpp

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -1,26 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int partition(int arr[], int lb, int ub)
+int part(int s, int e, int arr[])
 {
-    int st = lb;
-    int end = ub;
-    int pivot = arr[lb];
-    while (st < end)
+    int pind = s;
+    int pivot = arr[e];
+    // int n = sizeof(arr) / sizeof(arr[0]);
+    for (int i = s; i < e; i++)
     {
-        while (arr[st] <= pivot)
+        if (arr[i] < pivot)
         {
-            st++;
-        }
-        while (arr[end] > pivot)
-        {
-            end--;
-        }
-        if (st < end){
-            swap(arr[st], arr[end]);
+            swap(arr[pind], arr[i]);
+            pind++;
         }
     }
-    swap(arr[end], arr[lb]);
+    swap(arr[pind], arr[e]);
+    return pind;
+}
+void quicksort(int s, int e, int arr[])
+{
+    if (s < e)
+    {
+        int pind = part(s, e, arr);
+        quicksort(s, pind - 1, arr);
+        quicksort(pind + 1, e, arr);
+    }
 }
 int main()
 {
@@ -31,7 +35,7 @@ int main()
     {
         cin >> arr[i];
     }
-    partition(arr, 0, n);
+    quicksort(0, n - 1, arr);
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
